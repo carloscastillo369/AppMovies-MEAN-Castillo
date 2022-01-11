8f0b0d25935502baae1c2b1d8628b9a324ed6256
Carlos Castillo Cueva <ing.elec.castillo@gmail.com>
2022-01-11 15:51:27 -0500

    creación controladores, modelos y rutas  para productos

---
+++ server/src/controllers/products.js
@@ -0,0 +1,59 @@
+const Product = require('../models/product');
+
+exports.getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const products = await Product.find(id? { _id: id } : {});
+        res.json(products)
+        console.log(`Se obtuvo ${ id? 'el producto' : 'los productos' }`);
+
+    } catch (err) {
+        console.log('Hubo un error');
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.createProduct = async (req, res) => {
+    try {
+        const newProduct = req.body;
+        const product = new Product(newProduct);
+
+        await product.save();
+        res.send(product);
+        console.log('Nuevo producto creado');
+
+    } catch (err) {
+        console.log('Hubo un error');
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = req.body;
+        const { id } = req.params;
+    
+        const updatedProduct = await Product.findByIdAndUpdate( { _id: id }, product, { new: true })
+        res.json(updatedProduct);
+        console.log('Producto actualizado');
+
+    } catch (err) {
+        console.log('Hubo un error');
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        await Product.findByIdAndRemove({ _id: id });
+        res.json({ msg: 'Producto eliminado' });
+        console.log('Producto eliminado');
+
+    } catch (err) {
+        console.log('Hubo un error');
+        res.status(500).send('Hubo un error');
+    }
+}
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const conectDB = require('./database/db');
 const cors = require('cors');
 const config = require('./config');
+const productRouter = require('./routes/products');
 
 //Creamos el servidor
 const app = express();
@@ -10,6 +11,7 @@ conectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/products', productRouter);
 
 //Definimos ruta principal
 app.get('/', (req,res) => {
